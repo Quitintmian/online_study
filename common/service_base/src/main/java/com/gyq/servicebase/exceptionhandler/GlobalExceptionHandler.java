b3e73046d8fa8ed81d0a07f34c7152410756786c
lupetra <gyq@localgit.com>
2021-10-19 12:45:52 +0800

    service_edu 异常处理

---
+++ common/service_base/src/main/java/com/gyq/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.gyq.servicebase.exceptionhandler;
+
+import com.gyq.commonutils.ExceptionUtil;
+import com.gyq.commonutils.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    // 出现了什么异常就执行哪个方法，返回对应的json
+    @ExceptionHandler(Exception.class)
+    @ResponseBody // 为了返回数据
+    public R error(Exception e){
+        e.printStackTrace();
+        return R.error().message("执行了全局异常处理...");
+    }
+
+    // 自定义异常处理
+    @ExceptionHandler(GuliException.class)
+    @ResponseBody
+    public R error(GuliException e){
+        log.error(ExceptionUtil.getMessage(e)); // 向日志文件中添加异常信息
+        e.printStackTrace();
+        return R.error().code(e.getCode()).message(e.getMsg());
+    }
+}
